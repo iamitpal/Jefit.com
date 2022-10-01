c8476e96cdb91e18391871e4194de5c0cf39c390
iamitpal <amitpalinfo@yahoo.in>
2022-10-01 14:57:11 +0530

    DOM integration

---
+++ Script/Readmore.js
@@ -0,0 +1,16 @@
+document.querySelector("#Login").addEventListener("click", Login);
+document.querySelector("#Signup").addEventListener("click", Signup);
+document.querySelector("nav img").addEventListener("click", home);
+
+function Login() {
+  console.log("invoked");
+  window.location.href = "Login.html";
+}
+function Signup() {
+  console.log("invoked");
+  window.location.href = "Signup.html";
+}
+function home() {
+  console.log("invoked");
+  window.location.href = "nav.html";
+}
